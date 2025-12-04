20c2b739a41b6c1460219379a42beb2d438f9e1d
ThethirdWolf <thethirdwolfcontact1@gmail.com>
2025-12-04 16:12:59 +0100

    creation inf v2

--- src/create_information.php
+++ src/create_information.php
@@ -18,8 +18,14 @@ else {
         echo "<script>console.log('Erreur connexion BD');</script>";
     } else {
         echo "<script>console.log('Connecté à la BD !');</script>";
-        $sql_insert = "INSERT INTO $databases($databases) VALUES ('$info')";
-        if (mysqli_query($conn, $sql_insert)) {
+        $type = gettype($info);
+        $sql_insert = "INSERT INTO $databases VALUES (info=?)";
+        $stmt = mysqli_prepare($conn, $sql_insert);
+
+        mysqli_stmt_bind_param($stmt, $type,  $info);
+
+        //$sql_insert = "INSERT INTO $databases($databases) VALUES ('$info')";
+        if (mysqli_stmt_execute($stmt)) {
             echo "<script>console.log('Information ajoutée avec succès');</script>";
             header("Location: webadmin.php");
         } else {
