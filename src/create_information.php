2a1be5189523f1bd440cdc36b2e39e817615f0f7
ThethirdWolf <thethirdwolfcontact1@gmail.com>
2025-12-04 16:05:24 +0100

    creation inf v1

--- src/create_information.php
+++ src/create_information.php
@@ -1,1 +1,29 @@
 <?php
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db = "vines";
+$conn = mysqli_connect($host, $user, $pass);
+
+$databases = $_POST["add-info"];
+$info = $_POST["info"];
+
+if (!$conn) {
+    echo "<script>console.log('Erreur connexion serveur');</script>";
+}
+else {
+    echo "<script>console.log('Connecté au serveur !');</script>";
+    $base = mysqli_select_db($conn, $db);
+    if (!$base) {
+        echo "<script>console.log('Erreur connexion BD');</script>";
+    } else {
+        echo "<script>console.log('Connecté à la BD !');</script>";
+        $sql_insert = "INSERT INTO $databases($databases) VALUES ('$info')";
+        if (mysqli_query($conn, $sql_insert)) {
+            echo "<script>console.log('Information ajoutée avec succès');</script>";
+            header("Location: webadmin.php");
+        } else {
+            header("Location: webadmin.php?err=1");
+        }
+    }
+}
