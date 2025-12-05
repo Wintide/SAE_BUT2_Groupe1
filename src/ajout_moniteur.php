48ba28c5e77c20b0cd6f2bfc516360ec0f4a2e99
Haldien <48862782+Haldien@users.noreply.github.com>
2025-12-05 10:31:58 +0100

    Update ajout_moniteur.php

--- src/ajout_moniteur.php
+++ src/ajout_moniteur.php
@@ -1,5 +1,28 @@
 <?php
 
-echo "<p> MONITEUR </p>";
+// Connexion à la BD
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db = "vines";
+$conn = mysqli_connect($host, $user, $pass, $db);
 
+if (!$conn) {
+    echo "<script>console.log('Erreur connexion serveur');</script>";
+} else {
+    echo "<script>console.log('Connecté au serveur !');</script>";
+}
+
+$serial = $_POST["serial"];
+$manufacturer = $_POST["manufacturer"];
+$model = $_POST["model"];
+$size_inch = $_POST["size_inch"];
+$resolution = $_POST["resolution"];
+$connector = $_POST["connector"];
+$attached_to = $_POST["attached_to"];
+
+$insert_query = "INSERT INTO monitors (serial, manufacturer, model, size_inch, resolution, connector, attached_to)
+                VALUES ('$serial', '$manufacturer', '$model', '$size_inch', '$resolution', '$connector', '$attached_to')";
+
+$result = mysqli_query($conn, $insert_query);
 
