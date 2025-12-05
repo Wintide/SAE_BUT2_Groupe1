8cd7d86ea97e1c0d1e726068d913a018de6704c4
Wintide <thomas.peres03@gmail.com>
2025-12-05 13:25:02 +0100

    Reglage Modif equipement v2

--- src/script/inventaire.js
+++ src/script/inventaire.js
@@ -120,7 +120,7 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(this);
         let formData = new FormData(this);
 
-        fetch("modification_equipement.php", { method: "POST", body: formData })
+        fetch("src/modification_equipement.php", { method: "POST", body: formData })
             .then(res => res.json())
             .then(data => {
                 if (data.status === "success") {
