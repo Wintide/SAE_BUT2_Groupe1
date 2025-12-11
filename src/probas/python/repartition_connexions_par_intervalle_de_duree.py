01685a4eb50af08c3be79a5568bf6ee3f3f45a5f
Haldien <48862782+Haldien@users.noreply.github.com>
2025-12-11 15:52:43 +0100

    Add files via upload

---
+++ src/probas/python/repartition_connexions_par_intervalle_de_duree.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="vines"
+)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT MAX(duration_seconds) FROM users")
+max_value = cursor.fetchone()[0]
+
+intervalles = []
+debut = 0
+while debut < max_value:
+    fin = debut + 1000
+    intervalles.append((debut, fin))
+    debut = fin
+
+counts = []
+for start, end in intervalles:
+    cursor.execute("SELECT COUNT(*) FROM users WHERE duration_seconds >= " + debut + " AND duration_seconds < " + fin)
+    counts.append(cursor.fetchone()[0])
+
+cursor.close()
+
+labels = []
+for el in intervalles:
+    labels.append(el[0] + " - " + el[1] + "s")
+
+df = pd.DataFrame({
+    'categorie': labels,
+    'nombre': counts
+})
+
+plt.figure(figsize=(9, 9))
+plt.pie(df['nombre'], labels=df['categorie'], startangle=90)
+plt.title("Répartition des durées de connexions par tranches de 1000 secondes")
+plt.axis('equal')
+
+plt.savefig('../images/graphe.png')
+
+plt.show()
+plt.close()
