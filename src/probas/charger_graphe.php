d12cbc8bc60263c28e1e9f744f8dfd0758de7790
Wintide <thomas.peres03@gmail.com>
2025-12-11 14:48:58 +0100

    Lien vers page de stats

--- src/probas/charger_graphe.php
+++ src/probas/charger_graphe.php
@@ -1,9 +1,36 @@
+<?php session_start();
+if (empty($_SESSION['role']) ||$_SESSION['role'] !== "administrateur_web") {
+    header("Location: index.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Inventaire - Vines</title>
+    <link rel="stylesheet" href="css/style-adminweb.css">
+    <script src="script/chart.js"></script>
+</head>
 <body>
+<header>
+    <div class="header-content">
+        <img src="images/logovines.png" alt="Logo Vines" class="logo">
+        <nav>
+            <a href="index.php" class="center-link">Accueil</a>
+            <a href="webadmin.php" class="center-link">Admin web</a>
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
+            <div class="right-link">
+                <button id="userButton"><?= htmlspecialchars($_SESSION['login'], ENT_QUOTES, 'UTF-8') ?></button>
+                <div id="userOverlay" class="user-overlay" role="menu" aria-hidden="true">
+                    <a href="logout.php">Déconnexion</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
+<body>
+    <h1> Statistique </h1>
 <?php
 
 
