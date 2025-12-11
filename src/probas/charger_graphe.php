6e30fe9cf0fceee983802664b577623ba4d9bad3
Wintide <thomas.peres03@gmail.com>
2025-12-11 16:27:00 +0100

    Ajout des grpahe v1.8.4

--- src/probas/charger_graphe.php
+++ src/probas/charger_graphe.php
@@ -49,6 +49,7 @@ if (empty($_SESSION['role']) ||$_SESSION['role'] !== "administrateur_web") {
 $select = $_POST["stats"];
 
 $command = escapeshellcmd("../../sae/bin/python $select");
+echo $command;
 $output = shell_exec($command);
 
 ?>
