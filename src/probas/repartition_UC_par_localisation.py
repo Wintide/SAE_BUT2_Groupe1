bcdbdbd0dfcb40b328b6756519f0ed28bf9a787f
Haldien <48862782+Haldien@users.noreply.github.com>
2025-12-11 15:19:25 +0100

    Add files via upload
    Ajout de nouveaux scripts de génération de graphes

---
+++ src/probas/repartition_UC_par_localisation.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="vines"
+)
+
+cursor = db.cursor()
+cursor.execute("""
+    SELECT location, COUNT(*) AS compteur
+    FROM devices
+    GROUP BY location
+""")
+result = cursor.fetchall()
+
+cursor.close()
+
+df = pd.DataFrame(result, columns=['location', 'compteur'])
+
+plt.figure(figsize=(8, 8))
+plt.pie(df['compteur'], labels=df['location'], startangle=90)
+plt.title("Répartition des unités centrales par localisation")
+plt.axis('equal')
+
+plt.savefig('../images/graphe.png')
+
+plt.show()
+plt.close()
