fbac038331d54f49c231937b3650675677450dec
Haldien <48862782+Haldien@users.noreply.github.com>
2025-12-12 09:32:56 +0100

    Update repartition_moniteur_de.py

--- src/probas/python/repartition_moniteur_de.py
+++ src/probas/python/repartition_moniteur_de.py
@@ -46,6 +46,6 @@ plt.pie(values, labels=labels, startangle=90, autopct='%1.1f%%')
 plt.title(f"Répartition des moniteurs par {attribute}")
 plt.axis("equal")
 
-plt.savefig(f'../images/graphe.png')
+plt.savefig('../images/graphe.png')
 plt.show()
 plt.close()
