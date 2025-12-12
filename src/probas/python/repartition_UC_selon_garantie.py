c312841b32c569bde5e6e5b7ffccd7f4ff136743
Haldien <48862782+Haldien@users.noreply.github.com>
2025-12-12 16:19:01 +0100

    Add files via upload

---
+++ src/probas/python/repartition_UC_selon_garantie.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import mysql.connector
+from datetime import datetime
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="vines"
+)
+cursor = db.cursor()
+
+cursor.execute("""
+           SELECT warranty_end
+           FROM devices
+""")
+result = cursor.fetchall()
+
+cursor.close()
+
+df_dispositifs = pd.DataFrame(result, columns=['fin_garantie'])
+
+ajd = datetime.today()
+
+df_dispositifs['jours_restant'] = (pd.to_datetime(df_dispositifs['fin_garantie']) - ajd).dt.days
+
+def categorie_garantie(jours):
+    if jours <= 365:
+        return 'Dans 1 an'
+    elif jours <= 730:
+        return 'Dans 2 ans'
+    else:
+        return 'Plus de 2 ans'
+
+df_dispositifs['categorie_garantie'] = df_dispositifs['jours_restant'].apply(categorie_garantie)
+
+compter_garantie = df_dispositifs['categorie_garantie'].value_counts()
+
+plt.figure(figsize=(8, 8))
+plt.pie(compter_garantie, labels=compter_garantie.index, autopct='%1.1f%%', startangle=90)
+plt.title("Proximité à la date de fin de garantie des unités centrales")
+plt.axis('equal')
+
+plt.savefig('../images/graphe.png')
+
+plt.show()
+plt.close()
