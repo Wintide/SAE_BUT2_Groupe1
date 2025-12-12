16a6461caf82d63b2543502db986ec91c0506f19
Haldien <48862782+Haldien@users.noreply.github.com>
2025-12-12 15:25:56 +0100

    Update repartition_moniteur_de.py

--- src/probas/python/repartition_moniteur_de.py
+++ src/probas/python/repartition_moniteur_de.py
@@ -42,7 +42,7 @@ if len(labels) > 10:
     values = top_values
 
 plt.figure(figsize=(10, 10))
-plt.pie(values, labels=labels, startangle=90, autopct='%1.1f%%', pad=30) # pad : Plus d'espace entre titre et graphe pour affichage propre
+plt.pie(values, labels=labels, startangle=90, autopct='%1.1f%%', pad=30)
 plt.title(f"Répartition des moniteurs par '{attribute}'")
 plt.axis("equal")
 
