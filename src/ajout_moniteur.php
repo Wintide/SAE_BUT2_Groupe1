8a475d007874cf9000f22eb8ac940c73ea1b380e
ThethirdWolf <thethirdwolfcontact1@gmail.com>
2026-02-18 12:58:01 +0100

    Modification/correction sql ajout

--- src/ajout_moniteur.php
+++ src/ajout_moniteur.php
@@ -5,34 +5,51 @@ $host = "localhost";
 $user = "root";
 $pass = "root";
 $db = "vines";
+
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    echo "<script>console.log('Erreur connexion serveur');</script>";
+    die("Erreur connexion BDD : " . mysqli_connect_error());
+}
+$serial = isset($_POST["serial"]) ? $_POST["serial"] : null;
+$manufacturer = isset($_POST["manufacturer"]) ? $_POST["manufacturer"] : null;
+$model = isset($_POST["model"]) ? $_POST["model"] : null;
+$size_inch = isset($_POST["size_inch"]) ? $_POST["size_inch"] : null;
+$resolution = isset($_POST["resolution"]) ? $_POST["resolution"] : null;
+$connector = isset($_POST["connector"]) ? $_POST["connector"] : null;
+$attached_to = isset($_POST["attached_to"]) ? $_POST["attached_to"] : null;
+
+$stmt = mysqli_prepare($conn, "
+    INSERT INTO monitors
+    (serial, manufacturer, model, size_inch, resolution, connector, attached_to)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+");
+
+if ($stmt) {
+
+    mysqli_stmt_bind_param(
+        $stmt,
+        "sssisss",
+        $serial,
+        $manufacturer,
+        $model,
+        $size_inch,
+        $resolution,
+        $connector,
+        $attached_to
+    );
+
+    if (mysqli_stmt_execute($stmt)) {
+        header("Location: ajout_avec_formulaire.php?success=monitor");
+        exit();
+    } else {
+        echo "Erreur exécution : " . mysqli_stmt_error($stmt);
+    }
+
+    mysqli_stmt_close($stmt);
+
 } else {
-    echo "<script>console.log('Connecté au serveur !');</script>";
+    echo "Erreur préparation : " . mysqli_error($conn);
 }
 
-$serial = $_POST["serial"];
-$manufacturer = $_POST["manufacturer"];
-$model = $_POST["model"];
-$size_inch = $_POST["size_inch"];
-$resolution = $_POST["resolution"];
-$connector = $_POST["connector"];
-$attached_to = $_POST["attached_to"];
-
-$insert_query = "INSERT INTO monitors (serial, manufacturer, model, size_inch, resolution, connector, attached_to)
-                VALUES ('$serial', '$manufacturer', '$model', '$size_inch', '$resolution', '$connector', '$attached_to')";
-
-$result = mysqli_query($conn, $insert_query);
-
-if ($result) {
-    //echo "<script>console.log('Moniteur de numéro de série " . $serial . " ajouté avec succès')</script>";
-    header("Location: ajout_avec_formulaire.php");
-} else {
-    echo "Erreur : " . mysqli_error($conn);
-}
-
-
-
-
+mysqli_close($conn);
--- src/ajout_uc.php
+++ src/ajout_uc.php
@@ -5,41 +5,72 @@ $host = "localhost";
 $user = "root";
 $pass = "root";
 $db = "vines";
+
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    echo "<script>console.log('Erreur connexion serveur');</script>";
-} else {
-    echo "<script>console.log('Connecté au serveur !');</script>";
+    die("Erreur connexion BDD : " . mysqli_connect_error());
 }
 
-$name = $_POST["name"];
-$serial = $_POST["serial"];
-$manufacturer = $_POST["manufacturer"];
-$model = $_POST["model"];
-$type = $_POST["type"];
-$cpu = $_POST["cpu"];
-$ram_mb = $_POST["ram_mb"];
-$disk_gb = $_POST["disk_gb"];
-$os = $_POST["os"];
-$domain = $_POST["domain"];
-$location = $_POST["location"];
-$building = $_POST["building"];
-$room = $_POST["room"];
-$purchase_date = $_POST["purchase_date"];
-$macaddr = $_POST["macaddr"];
-$warranty_end = $_POST["warranty_end"];
+$name = isset($_POST["name"]) ? $_POST["name"] : null;
+$name = isset($_POST["name"]) ? $_POST["name"] : null;
+$serial = isset($_POST["serial"]) ? $_POST["serial"] : null;
+$manufacturer = isset($_POST["manufacturer"]) ? $_POST["manufacturer"] : null;
+$model = isset($_POST["model"]) ? $_POST["model"] : null;
+$type = isset($_POST["type"]) ? $_POST["type"] : null;
+$cpu = isset($_POST["cpu"]) ? $_POST["cpu"] : null;
+$ram_mb = isset($_POST["ram_mb"]) ? $_POST["ram_mb"] : null;
+$disk_gb = isset($_POST["disk_gb"]) ? $_POST["disk_gb"] : null;
+$os = isset($_POST["os"]) ? $_POST["os"] : null;
+$domain = isset($_POST["domain"]) ? $_POST["domain"] : null;
+$location = isset($_POST["location"]) ? $_POST["location"] : null;
+$building = isset($_POST["building"]) ? $_POST["building"] : null;
+$room = isset($_POST["room"]) ? $_POST["room"] : null;
+$purchase_date = isset($_POST["purchase_date"]) ? $_POST["purchase_date"] : null;
+$macaddr = isset($_POST["macaddr"]) ? $_POST["macaddr"] : null;
+$warranty_end = isset($_POST["warranty_end"]) ? $_POST["warranty_end"] : null;
 
-$insert_query = "INSERT INTO devices (name, serial, manufacturer, model, type, cpu, ram_mb, disk_gb, os, domain, location, building, room, purchase_date, macaddr, warranty_end)
-          VALUES ('$name', '$serial', '$manufacturer', '$model', '$type', '$cpu', '$ram_mb', '$disk_gb', '$os', '$domain', '$location', '$building', '$room', '$purchase_date', '$macaddr', '$warranty_end')";
 
-$result = mysqli_query($conn, $insert_query);
+$stmt = mysqli_prepare($conn, "
+    INSERT INTO devices
+    (name, serial, manufacturer, model, type, cpu, ram_mb, disk_gb, os, domain, location, building, room, purchase_date, macaddr, warranty_end)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+");
 
-if ($result) {
-    //echo "<script>alert('Unité centrale ". $name ." ajoutée avec succès');</script>";
-    header("Location: ajout_avec_formulaire.php");
+if ($stmt) {
+
+    mysqli_stmt_bind_param(
+        $stmt,
+        "ssssssiiisssssss",
+        $name,
+        $serial,
+        $manufacturer,
+        $model,
+        $type,
+        $cpu,
+        $ram_mb,
+        $disk_gb,
+        $os,
+        $domain,
+        $location,
+        $building,
+        $room,
+        $purchase_date,
+        $macaddr,
+        $warranty_end
+    );
+
+    if (mysqli_stmt_execute($stmt)) {
+        header("Location: ajout_avec_formulaire.php?success=uc");
+        exit();
+    } else {
+        echo "Erreur exécution : " . mysqli_stmt_error($stmt);
+    }
+
+    mysqli_stmt_close($stmt);
+
 } else {
-    echo "Erreur : " . mysqli_error($conn);
+    echo "Erreur préparation : " . mysqli_error($conn);
 }
 
-
+mysqli_close($conn);
