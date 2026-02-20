b23bbc7cdf908e0ad2384433336845053708e375
Thomas <thomas.aymard1@gmail.com>
2026-02-20 11:04:13 +0100

    filtre search

--- src/inventaire.php
+++ src/inventaire.php
@@ -162,7 +162,7 @@ if (empty($_SESSION['role']) ||$_SESSION['role'] !== "technicien") {
                                 $req = $req . ";";
                                 if($table == "monitors"){
                                     charge_monitors_from_req($conn, $req);
-                                } else if ($table == null){
+                                } else if ($_POST['filter-type'] == "all"){
                                     charge_devices_from_req($conn, $req);
                                     charge_monitors_from_req($conn, $req2);
                                 }
