01d1813df996e4d4696080fecacb0e3a747694f5
Wintide <thomas.peres03@gmail.com>
2026-04-09 10:39:53 +0200

    debug des logs ssh

--- src/sysadmin.php
+++ src/sysadmin.php
@@ -73,8 +73,8 @@ if (empty($_SESSION['role']) ||$_SESSION['role'] !== "administrateur_systeme") {
         require_once 'log_utils.php';
         $command_reussi = escapeshellcmd('cat ../../../../log/auth.log | grep Accepted');
         $command_rate = escapeshellcmd('cat ../../../../log/auth.log | grep Failed');
-        $output_reussi = trim(shell_exec($command_reussi));
-        $output_rate = trim(shell_exec($command_rate));
+        $output_reussi = shell_exec($command_reussi);
+        $output_rate = shell_exec($command_rate);
         echo $output_reussi;
         $output_reussi = str_replace(array("\r", "\n"), '', $output_reussi);
         $output_rate = str_replace(array("\r", "\n"), '', $output_rate);
