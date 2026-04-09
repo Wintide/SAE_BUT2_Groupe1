92ecbd216baf47b3c96652fe4d23a20ebfd8574a
Wintide <thomas.peres03@gmail.com>
2026-04-09 10:30:33 +0200

    ajout du chargement des logs ssh

--- src/sysadmin.php
+++ src/sysadmin.php
@@ -70,6 +70,34 @@ if (empty($_SESSION['role']) ||$_SESSION['role'] !== "administrateur_systeme") {
     <section class="log-connexion-ssh">
         <h1>Connexion SSH</h1>
         <?php
+        require_once 'log_utils.php';
+        $command_reussi = escapeshellcmd('cat ../../../../log/auth.log | grep Accepted');
+        $command_rate = escapeshellcmd('cat ../../../../log/auth.log | grep Failed');
+        $output_reussi = trim(shell_exec($command_reussi));
+        $output_rate = trim(shell_exec($command_rate));
+
+        $output_reussi = str_replace(array("\r", "\n"), '', $output_reussi);
+        $output_rate = str_replace(array("\r", "\n"), '', $output_rate);
+
+        foreach ($output_reussi as $ligne) {
+            $nouveau = [
+                    "status" => "réussi",
+                    "date" => $ligne[0],
+                    "ip" => $ligne[8],
+                    "port" => $ligne[10]
+            ];
+            ecrireLogJson('logs/connexion_ssh.json',$nouveau);
+        }
+
+        foreach ($output_rate as $ligne) {
+            $nouveau = [
+                    "status" => "échec",
+                    "date" => $ligne[0],
+                    "ip" => $ligne[8],
+                    "port" => $ligne[10]
+            ];
+            ecrireLogJson('logs/connexion_ssh.json',$nouveau);
+        }
         $logFile = 'logs/connexions_ssh.json';
         if (file_exists($logFile)) {
             $log = file_get_contents($logFile);
